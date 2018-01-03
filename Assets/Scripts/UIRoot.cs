14f784471a949036bb894b3ce8492322293bf4a3
yc2768 <2768647687@qq.com>
2018-01-03 23:09:06 +0800

    LearnFairyGUI
    测试包裹canvas

---
+++ Assets/Scripts/UIRoot.cs
@@ -0,0 +1,37 @@
+﻿using FairyGUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRoot : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start () {
+        //设置分辨率
+        GRoot.inst.SetContentScaleFactor(600, 480);
+        UIPackage.AddPackage("UI/主菜单");
+        //创建组件
+         GComponent Comp = UIPackage.CreateObject("主菜单", "测试组件").asCom;
+        // GRoot.inst.AddChild(Comp);
+
+        Object canvasObjectprefab = Resources.Load("Canvas");
+        GameObject canvasObject = (GameObject)Object.Instantiate(canvasObjectprefab);
+       
+
+        canvasObject.transform.localPosition = new Vector3(296, -514, 0);
+        canvasObject.transform.localScale = new Vector3(1, 1, 1);
+        canvasObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+        //GGraph holder = view.GetChild("holder").asGraph;
+        GGraph holder=Comp.GetChild("n1").asGraph;
+        GoWrapper gw = new GoWrapper(canvasObject);
+
+        holder.SetNativeObject(gw);
+        GRoot.inst.AddChild(Comp);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/setAngle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setAngle : MonoBehaviour {
+    float value = 60f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addAngle()
+    {
+         value+= 10f;
+        GetComponent<Image>().material.SetFloat("_Angle",value);
+    }
+}
